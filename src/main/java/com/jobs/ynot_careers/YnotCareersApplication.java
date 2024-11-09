7b81d5cfd7f39fbcc25300d1da8abf63d2870438
SaiSeshu <68633817+seshubabubatchu@users.noreply.github.com>
2024-11-09 14:25:22 +0530

    project intialization

---
+++ src/main/java/com/jobs/ynot_careers/YnotCareersApplication.java
@@ -0,0 +1,13 @@
+package com.jobs.ynot_careers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnotCareersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnotCareersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobs/ynot_careers/YnotCareersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.ynot_careers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnotCareersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
